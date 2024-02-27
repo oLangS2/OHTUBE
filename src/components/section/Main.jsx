d32767d8c518ea5b56f8553d0fe013755521cbd2
guswjd8389@gmail.com <guswjd8389@gmail.com>
2024-02-27 15:40:32 +0900

    컴포넌트 비동기 작업

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -1,10 +1,17 @@
-import React from "react"
+import React from 'react'
 
-const Main = () =>{
-    return(
-        <main id ="main" role="main">
-            main
-        </main>
+import Header from './Header'
+import Footer from './Footer'
+
+const Main = ( props ) => {
+    return (
+        <>
+            <Header />
+            <main id="main" role="main">
+                {props.children}
+            </main>
+            <Footer />
+        </>
     )
 }
 

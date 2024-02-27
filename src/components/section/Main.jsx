a8947b27d1841ee314ae4ff274ff5b0cc67758ae
guswjd8389@gmail.com <guswjd8389@gmail.com>
2024-02-27 16:07:26 +0900

    페이지 SEO 작업

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
+import { Helmet,HelmetProvider } from 'react-helmet-async'
 
 import Header from './Header'
 import Footer from './Footer'
 
 const Main = ( props ) => {
     return (
-        <>
+        <HelmetProvider>
+            <Helmet titleTemplate='%s | OHTUBE'
+            defaultTitle='OHTUBE'
+            defer={false}>
+                {props.title&&<title>{props.title}</title>}
+                <meta name ="desciption" content={props.description}/>
+            </Helmet>
             <Header />
             <main id="main" role="main">
                 {props.children}
             </main>
             <Footer />
-        </>
+        </HelmetProvider>
     )
 }
 

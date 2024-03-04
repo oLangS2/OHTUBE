78f2199702dc8d53d23f4189868165c7847bcc2a
guswjd8389@gmail.com <guswjd8389@gmail.com>
2024-03-04 16:54:41 +0900

    youtube와 Rapid Api 이용과 더보기 설정하기

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Search = () => {
+    const [searchKeyword,setSearchKEyword]= useState(""); // 초기값을 빈 문자열로 설정하고 searchKeyword 라는 변수는 사용자가 입력한 검색어를 저장합니다.
+    const navigate = useNavigate();
+
+    const handleSearch =() => {
+        console.log(searchKeyword)
+        if(searchKeyword){
+            navigate(`/search/$[searchKeyword]`);
+            setSearchKEyword("");
+        }
+    };
+
     return (
         <div id='search'>
             <div className='search__inner'>
@@ -13,6 +25,12 @@ const Search = () => {
                     placeholder='검색어를 입력해주세요!' 
                     autoComplete='off' 
                     className='search__input' 
+                    onChange={e=>setSearchKEyword(e.target.value)}
+                    onkeyDown={e=>{
+                        if(e.key==='Enter'){
+                            handleSearch();
+                        }
+                    }}
                 />
             </div>
         </div>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import Main from '../components/section/Main'
 
+import VideoSearch from '../components/videos/VideoSearch'
+import { fetchFromAPI } from '../utils/api'
+
 const Search = () => {
-    return(
+    const { searchId } = useParams();
+    const [ videos, setVideos ] = useState([]);
+    const [ nextPageToken, setNextPageToken ] = useState(null);
+    const [ loading, setLoading ] = useState(true); 
+    
+    useEffect(() => {
+        setVideos([]);
+        fetchVideos(searchId);
+        setLoading(true);
+    }, [searchId]);
+
+    const fetchVideos = (query, pageToken = '') => {
+        fetchFromAPI(`search?part=snippet&q=${query}&pageToken=${pageToken}`)
+            .then((data) => {
+                setNextPageToken(data.nextPageToken);
+                setVideos((prevVideos) => [...prevVideos, ...data.items]);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+                setLoading(false); 
+            });
+    };
+
+    const handleLoadMore = () => {
+        if (nextPageToken) {
+            fetchVideos(searchId, nextPageToken);
+        }
+    };
+
+    const searchPageClass = loading ? 'isLoading' : 'isLoaded';
+
+    return (
         <Main 
-            title = "유튜브 검색"
+            title = "유투브 검색"
             description="유튜브 검색 결과 페이지입니다.">
-            Search
+            
+            <section id='searchPage' className={searchPageClass}>
+                <h2><em>{searchId}</em> 검색 결과입니다.</h2>
+                <div className="video__inner search">
+                    <VideoSearch videos={videos} />
+                </div>
+                <div className="video__more">
+                    {nextPageToken && (
+                        <button onClick={handleLoadMore}>더 보기</button>
+                    )}
+                </div>
+            </section>
         </Main>
     )
 }
 
 export default Search
+
+
+

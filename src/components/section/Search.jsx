52af09ebaebefd4b07c2cb92ddc12c6367a2d39d
guswjd8389@gmail.com <guswjd8389@gmail.com>
2024-03-04 16:58:55 +0900

    비디오 상세 페이지 만들기

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -2,15 +2,15 @@ import React,{useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Search = () => {
-    const [searchKeyword,setSearchKEyword]= useState(""); // 초기값을 빈 문자열로 설정하고 searchKeyword 라는 변수는 사용자가 입력한 검색어를 저장합니다.
+    const [searchKeyword,setSearchKEyword]= useState(""); // 초기값을 빈 문자열로 설정하고 searchKeyword 라는 변수는 사용자가 입력한 검색어를 저장한다.
     const navigate = useNavigate();
 
-    const handleSearch =() => {
+    const handleSearch =() => { 
         console.log(searchKeyword)
         if(searchKeyword){
             navigate(`/search/$[searchKeyword]`);
             setSearchKEyword("");
-        }
+        } //handleSearch 함수는 검색 버튼을 클릭하거나 Enter버튼을 눌렀을 떄 호출된다. 검색어가 있는 경우에만 내비게이션을 수행 후 초기화 한다.
     };
 
     return (
